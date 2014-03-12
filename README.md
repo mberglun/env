ca735398a10daf491754ac432e4605344aed161f
mberglun <mathias.berglund@iki.fi>
2014-03-12 15:16:18 +0200

    Updated

---
+++ README.md
@@ -0,0 +1,8 @@
+env
+===
+
+Vim setup for Ubuntu.
+
+# Install
+
+    curl -L https://github.com/mberglun/env/raw/master/install.sh | zsh
--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ ENVDIR=$HOME/.env
 test -d $ENVDIR || git clone git://github.com/mberglun/env.git $ENVDIR
 find $ENVDIR -type f -name .\* -exec ln -is {} $HOME \;
 
+# Setup vim
+d=~/.vim/bundle
+mkdir -p $d
+test -d $d/neobundle.vim || git clone git://github.com/Shougo/neobundle.vim $d/neobundle.vim
+vim +NeoBundleInstall! +q
